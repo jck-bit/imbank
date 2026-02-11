091b67e96a55a01a243cc3997df5e3ffddeb59e5
Jack Kinyanjui Wambui <jack.kinyanjui@KEMDGF1644.local>
2026-02-11 22:49:49 +0300

    feat: Adds a liquibase precommit hook

--- src/main/java/com/example/imbank/ImbankApplication.java
+++ src/main/java/com/example/imbank/ImbankApplication.java
@@ -2,10 +2,8 @@ package com.example.imbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ImbankApplication {
 
     public static void main(String[] args) {
